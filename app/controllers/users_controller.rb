3605a09f1151d6c41e8834853671db00adb9687a
Ian Lozinski <ian.lozinski@gmail.com>
2013-12-02 01:34:55 -0500

    Users being created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:email).permit(:password)
+      params.require(:user).permit(:email, :password)
     end
 end
