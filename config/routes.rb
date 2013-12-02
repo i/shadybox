2223ce618571521440a0db3bd8238d8bffbdf48e
Ian Lozinski <ian.lozinski@gmail.com>
2013-12-02 05:22:00 -0500

    Added sessions. Basic auth done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ShadyBox::Application.routes.draw do
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  resources :sessions
+
   get "sign_up" => "users#new", :as => "sign_up"
   root "users#new"
   resources :users
