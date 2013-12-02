782dbef650a3ecc554476ed1e067cce0ceba4f8a
Ian Lozinski <ian.lozinski@gmail.com>
2013-12-01 22:02:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShadyBox::Application.config.secret_key_base = '772d0d6c46722d0daa876a83942cc702b203b010168018524f8e0ee29f82386690805fcda04a73cd3ba04eb42cccce12e8f8daa90d23862fcf066528fe9b98e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShadyBox::Application.config.session_store :cookie_store, key: '_ShadyBox_session'
